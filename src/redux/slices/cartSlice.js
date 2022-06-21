10bed2c9fd7cafe2bb51cc5292f40916d3ab5ebe
el ganso <elganso45@gmail.com>
2022-06-21 14:26:20 +0300

    фикс бага при удалении товара из корзины

--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -27,10 +27,16 @@ export const cartSlice = createSlice({
       const findItem = state.items.find((obj) => obj.id === action.payload);
       if (findItem) {
         findItem.count--;
+        state.totalPrice = state.items.reduce((sum, obj) => {
+          return obj.price * obj.count - sum;
+        }, 0);
       }
     },
     removeItem(state, action) {
       state.items = state.items.filter((obj) => obj.id !== action.payload);
+      state.totalPrice = state.items.reduce((sum, obj) => {
+        return obj.price * obj.count - sum;
+      }, 0);
     },
     clearItems(state) {
       state.items = [];
